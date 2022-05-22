fd9ac08810508173eef53839ae2fb59396c01ae0
DESKTOP-37OOLVR\ASRock-lightning <hxh7639@yahoo.com.tw>
2022-05-21 21:38:08 -0500

    basic UI and code start

---
+++ Assets/Scripts/BeatChecker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatChecker : MonoBehaviour
+{
+    [SerializeField] private AudioClip _audioClip = null;
+    [SerializeField] private AudioSource _audioSource = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
