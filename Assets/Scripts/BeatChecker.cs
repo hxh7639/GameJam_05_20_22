33f1008a531cb00fbfd6ba6ea5b68d11c5a2ceb1
DESKTOP-37OOLVR\ASRock-lightning <hxh7639@yahoo.com.tw>
2022-05-23 00:02:26 -0500

    beatChecker updated, bug fixed.

--- Assets/Scripts/BeatChecker.cs
+++ Assets/Scripts/BeatChecker.cs
@@ -9,23 +9,29 @@ public class BeatChecker : MonoBehaviour
 {
     [SerializeField] private AudioClip _audioClip = null;
     [SerializeField] private AudioSource _audioSource = null;
-    [SerializeField] private float _time;
-    [SerializeField] private float _beatTimer;
 
-    [SerializeField] private float _firstBeatTimer = 0;
-    [SerializeField] private float _beatsPerMin = 140;
-    [SerializeField] private float _secPerBeat = 0;
+    [Header("Settings")]    
+    [SerializeField] private float _beatsPerMin = 140;    
     [SerializeField] private Image _tempImage;
-    [SerializeField] private float _hitGraceTimer = 0.3f;
-                     private float _scriptHitGraceTimer = 0.1f;
-    [SerializeField] private bool _beatStarted = false;
+    [SerializeField] private int _beatScale = 4;
+    [SerializeField] private double _hitGraceTimer = 0.3f;
+    [SerializeField] private float _first4thBeatTimer = 0;
+
+    [Header("info only")] 
+    [SerializeField] private float _secPerBeat = 0;
+    [SerializeField] private double _graceTimer;
+    [SerializeField] private double _currentTime;
+    [SerializeField] private double _songStartedTime = 0;
+    [SerializeField] private double _currentSongPosition = 0;
+    [SerializeField] private bool _isBeatStarted = false;
+    [SerializeField] private double _lastBeatTimer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         _audioSource.clip = _audioClip;
         _secPerBeat = 60 / _beatsPerMin;
-        _secPerBeat = _secPerBeat * 4;
+        _secPerBeat = _secPerBeat * _beatScale;
         
         
     }
@@ -38,50 +44,50 @@ public class BeatChecker : MonoBehaviour
             StartGame();
         }
         if(!_audioSource.isPlaying){return;}
-        if(!_beatStarted)
-        {
-            _firstBeatTimer -=Time.deltaTime;
 
-            if(_firstBeatTimer <= 0)
-            {
-                _beatStarted = true;
-                _beatTimer = -1;
-                TempCountBeat();
-            }
-            return;
-        }
-
-        if(_scriptHitGraceTimer < 0 )
-        {
-            _tempImage.color = Color.black;
-        }
-
-
-        _beatTimer -= Time.deltaTime;  
-        _scriptHitGraceTimer -= Time.deltaTime;
-        
-
-        if(_beatTimer < 0)
-        {
-            TempCountBeat();
-        }
-        
-
-
+        TempCountBeat3();
 
     }
 
     private void StartGame()
     {
         _audioSource.Play();
-        _beatTimer = _secPerBeat;
+        _songStartedTime = Time.timeAsDouble;
     }
 
-    private void TempCountBeat()
+    private void TempCountBeat3()
     {
-        Debug.Log("beat counted");
-        _beatTimer = _secPerBeat;
-        _tempImage.color = Color.green;
-        _scriptHitGraceTimer = _hitGraceTimer;
+        _currentTime = Time.timeAsDouble;
+
+        _currentSongPosition = _currentTime - _songStartedTime;
+
+        if (!_isBeatStarted && (_first4thBeatTimer) <= _currentSongPosition)
+        {
+            Debug.Log("beat counted, 1st beat");
+            _lastBeatTimer = Time.timeAsDouble;
+            _isBeatStarted = true;
+        }
+
+        if(!_isBeatStarted)
+        {
+            return;
+        }
+
+        if(_currentTime > _graceTimer) 
+        {
+            _tempImage.color = Color.black;
+        }
+
+
+        if( (_currentTime - _lastBeatTimer) > (_secPerBeat))
+        {
+            Debug.Log("record new beat");
+            _tempImage.color = Color.green;
+            _lastBeatTimer = Time.timeAsDouble;
+            _graceTimer = Time.timeAsDouble + _hitGraceTimer;
+        }
+
+
+
     }
 }
